55fb202b9ff8d6101e184b13e89856f10ff1753b
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-03-19 16:24:10 +0600

    Add files via upload

---
+++ 1080.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,max=-1,p;
+    for(i=1;i<101;i++){
+        scanf("%d",&n);
+        if(max<n)
+            max=n,p=i;
+        }
+    printf("%d\n%d\n",max,p);
+    return 0;
+}
---
+++ 1132.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+int x,y,i,sum=0;
+scanf("%d %d",&x,&y);
+if(y>x){
+for(i=x;i<=y;i++){
+    if(i%13!=0)
+        sum=sum+i;
+    }
+}
+else if(x>y){
+    for(i=y;i<=x;i++){
+        if(i%13!=0)
+        sum=sum+i;
+        }
+    }
+    printf("%d\n",sum);
+return 0;
+}
