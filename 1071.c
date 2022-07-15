415c8011fd8fa5ecc8c4b312295613bd6230fb52
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2022-07-15 22:45:26 +0600

    Add files via upload

--- 1071.c
+++ 1071.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
-int main(){
-    int X, Y, i;
-    scanf("%d%d", &X,&Y);
-    if (X > Y) {
-         for (i = Y+1 ; i < X; i++) {
-                if (i % 5 == 2 || i % 5 == 3) {
-                    printf("%d\n", i);
-                }
-            }
-    }else if(X < Y){
-        for (i = X+1 ; i < Y; i++) {
-                if (i % 5 == 2 || i % 5 == 3) {
-                     printf("%d\n", i);
-                }
-            }
+int main()
+{
+    int X, Y, i, s=0;
+    scanf("%d %d",&X,&Y);
+    for(i=Y+1; i<X; i++)
+    {
+        if(i%2!=0)
+        {
+            s+=i;
+        }
     }
-
+    printf("%d\n",s);
+    return 0;
 }
